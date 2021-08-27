fecb67e1af3f844c9548b1b3e07f7f9298d1ae4e
Hans Kyndesgaard <hans@kyndesgaard.dk>
2021-08-27 22:08:59 +0200

    Confirm reload

--- src/index.js
+++ src/index.js
@@ -225,19 +225,19 @@ class Game extends React.Component {
         );
     }
 
-    // componentDidMount() {
-    //     window.addEventListener('beforeunload', this.beforeunload.bind(this));
-    // }
+    componentDidMount() {
+        window.addEventListener('beforeunload', this.beforeunload.bind(this));
+    }
 
-    // componentWillUnmount() {
-    //     window.removeEventListener('beforeunload', this.beforeunload.bind(this));
-    // }
+    componentWillUnmount() {
+        window.removeEventListener('beforeunload', this.beforeunload.bind(this));
+    }
 
-    // beforeunload(e) {
-    //     console.log('Before unload');
-    //     e.preventDefault();
-    //     e.returnValue = true;
-    // }
+    beforeunload(e) {
+        console.log('Before unload');
+        e.preventDefault();
+        e.returnValue = true;
+    }
 }
 
 // ========================================
